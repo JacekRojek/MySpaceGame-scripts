c2703680862b3790535322cf55e5809d7aa60ff0
JacekRojek <j.rojek19@gmail.com>
2016-04-20 13:13:14 +0100

    JSON Update

---
+++ JSON/Crew/CharacterData.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+using UnityEngine.UI;
+public class CharacterData : MonoBehaviour,IPointerEnterHandler, IPointerExitHandler
+{
+    public CharacterToPick character;
+    public int amount;
+    private TooltipCharacter tooltip;
+    private GameObject panel;
+    private GameObject ChosedPanel;
+    private GameObject playButton;
+    
+
+    void Start()
+    {
+        playButton = this.transform.Find("/Canvas/Button_Play").gameObject;
+        ChosedPanel = this.transform.Find("/Canvas/CapitanSelection_Panel/selected_crew").gameObject;
+        panel = this.transform.Find("/Canvas/CapitanSelection_Panel/CreqPanel").gameObject;
+        tooltip = panel.GetComponent<TooltipCharacter>();
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+
+        tooltip.Activate(character);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltip.Disactivate();
+    }
+    public void OnClick()
+    {
+        ChosedPanel.transform.GetChild(0).GetComponent<Image>().sprite = this.GetComponent<Image>().sprite;
+        playButton.GetComponent<Button>().interactable = true;
+    }
+}
---
+++ JSON/Crew/Characters_JSONC.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+public class Characters_JSONC : MonoBehaviour {
+    int slotamount;
+    GameObject slotPanel;
+    public GameObject inventorySlot;
+    public GameObject inventoryItem;
+    CharactersDatabase database;
+    public List<CharacterToPick> items = new List<CharacterToPick>();
+    public List<GameObject> slots = new List<GameObject>();
+
+    void Start()
+    {
+        database = this.GetComponent<CharactersDatabase>();
+        slotamount = 6;
+        slotPanel = this.gameObject;
+        for (int i = 0; i < slotamount; i++)
+        {
+            items.Add(new CharacterToPick());
+            slots.Add(Instantiate(inventorySlot));
+            slots[i].transform.SetParent(slotPanel.transform);
+        }
+
+        AddItem(0);
+        AddItem(1);
+        AddItem(2);
+        AddItem(3);
+    }
+
+    public void AddItem(int id)
+    {
+        CharacterToPick itemToAdd = database.FetchItembyID(id);
+
+      
+            for (int i = 0; i < items.Count; i++)
+            {
+            if (items[i].ID == -1)
+            {
+                items[i] = itemToAdd;
+                GameObject itemObject = Instantiate(inventoryItem);
+                itemObject.GetComponent<CharacterData>().character = itemToAdd;
+                itemObject.transform.SetParent(slots[i].transform);
+                itemObject.transform.position = Vector2.zero;
+                itemObject.GetComponent<Image>().sprite = itemToAdd.sprite;
+                itemObject.name = itemToAdd.Title;
+                itemObject.GetComponent<CharacterData>().character = itemToAdd;
+                Debug.Log(itemObject.GetComponent<CharacterData>().character.ID);
+                break;
+            }
+        }
+    }
+    bool CheckInventory(CharacterToPick item)
+    {
+        for (int i = 0; i < items.Count; i++)
+        {
+            if (items[i].ID == item.ID)
+                return true;
+        }
+        return false;
+    }
+}
